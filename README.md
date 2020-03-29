aac7c27c82015a3edf5c1f18402eac6aeac8f1bc
Colin Dembovsky <colindembovsky@gmail.com>
2020-03-29 11:02:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Release Azure Web Apps with A/B Testing using GitHub Actions Sample
+
---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_app_actions_deploy_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
